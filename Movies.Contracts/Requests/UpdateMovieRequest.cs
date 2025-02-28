b6b7ab8eea6e5cc874683cbc013c58effcb0f132
Luke <lukebielecki@gmail.com>
2025-02-28 19:46:39 +0100

    Add project files.

---
+++ Movies.Contracts/Requests/UpdateMovieRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Contracts.Requests
+{
+    public class UpdateMovieRequest
+    {
+        public required string Title { get; init; }
+        public required string YearOfRelease { get; set; }
+        public required IEnumerable<string> Genres { get; set; } = Enumerable.Empty<string>();
+    }
+}
