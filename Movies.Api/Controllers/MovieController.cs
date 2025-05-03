edb693bb9dc80edead53a01c2d92d4aec88c4d2d
Luke <lukebielecki@gmail.com>
2025-05-04 00:02:23 +0200

    03_06 wip

---
+++ Movies.Api/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+[Route("api")]
+public class MovieController : ControllerBase
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MovieController(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+
+    [HttpPost("movies")]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        return Ok(request);
+    }
+}
--- Movies.Contracts/Requests/CreateMovieRequest.cs
+++ Movies.Contracts/Requests/CreateMovieRequest.cs
@@ -9,7 +9,7 @@ namespace Movies.Contracts.Requests
     public class CreateMovieRequest
     {
         public required string Title { get; init; }
-        public required string YearOfRelease { get; set; }
+        public required int YearOfRelease { get; set; }
         public required IEnumerable<string> Genres { get; set; } = Enumerable.Empty<string>();
     }
 }
