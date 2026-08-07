f2c3d09b6120e373d89bc4e9af9b71f750bcd754
Luke <lukebielecki@gmail.com>
2026-08-07 11:18:12 +0200

    03_07 mapping

--- Movies.Api/Controllers/MovieController.cs
+++ Movies.Api/Controllers/MovieController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Models;
 using Movies.Application.Repositories;
 using Movies.Contracts.Requests;
 
@@ -18,6 +20,11 @@ public class MovieController : ControllerBase
     [HttpPost("movies")]
     public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
     {
-        return Ok(request);
+        var movie = request.MapToMovie();
+
+        var result = await _movieRepository.CreateAsync(movie);
+
+        //return Ok(movie);
+        return Created($"/api/movies/{movie.Id}", movie);
     }
 }
---
+++ Movies.Api/Mapping/ContractMapping.cs
@@ -0,0 +1,21 @@
+﻿using Movies.Application.Models;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Mapping
+{
+    public static class ContractMapping
+    {
+        public static Movie MapToMovie(this CreateMovieRequest request)
+        {
+            var movie = new Movie
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title,
+                YearOfRelease = request.YearOfRelease,
+                Genres = request.Genres.ToList()
+            }; 
+            
+            return movie;
+        }
+    }
+}
